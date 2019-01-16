dd960f49500bc10dc3ebffe74dae34d53849d295
arthur0420 <arthur0420@aliyun.com>
2019-01-16 23:22:34 +0800

    准备提交。

--- src/arthur/feedingControl/functions/Function1022.java
+++ src/arthur/feedingControl/functions/Function1022.java
@@ -1,26 +1,10 @@
 package arthur.feedingControl.functions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import net.sf.json.JSONArray;
 
 import org.apache.log4j.Logger;
 
 import arthur.feedingControl.entity.Result;
-import arthur.feedingControl.service.ApartmentService;
-import arthur.feedingControl.service.ApartmentServiceImp;
-import arthur.feedingControl.service.BaseService;
-import arthur.feedingControl.service.CellsService;
-import arthur.feedingControl.service.CellsServiceImp;
-import arthur.feedingControl.service.ConstantsService;
-import arthur.feedingControl.service.ConstantsServiceImp;
-import arthur.feedingControl.service.LogService;
-import arthur.feedingControl.service.LogServiceImp;
-import arthur.feedingControl.service.ScheduleService;
-import arthur.feedingControl.service.ScheduleServiceImp;
 import arthur.feedingControl.service.UserService;
 import arthur.feedingControl.service.UserServiceImp;
 import arthur.feedingControl.utils.ConvertHelper;
--- src/arthur/feedingControl/main/LaunchProcessor.java
+++ src/arthur/feedingControl/main/LaunchProcessor.java
@@ -2,6 +2,7 @@ package arthur.feedingControl.main;
 
 import java.util.Calendar;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import arthur.feedingControl.utils.Config;
 import arthur.feedingControl.utils.DailyTask;
@@ -22,6 +23,21 @@ public class LaunchProcessor extends Thread{
 		first.set(Calendar.MILLISECOND, 0);
 		//-----------------------定时任务的部分
 		Timer t1 = new Timer();// 每天更新config
-		t1.schedule(new DailyTask(),first.getTime() , 24*60*60*1000);
+		t1.scheduleAtFixedRate(new DailyTask(),first.getTime() , 24*60*60*1000);
+		
+		
+		Calendar cal = Calendar.getInstance();
+//		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int mi = cal.get(Calendar.MINUTE);
+		long delay = 0;
+		delay = (60 - mi)*60 * 1000 ;
+		Timer t2 = new Timer();
+		t2.scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				
+			}	
+		}, delay, 60 *60 * 1000);
 	}
 }
